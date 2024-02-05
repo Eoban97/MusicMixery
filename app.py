a3cfe0601df259a9156f69e5c3acd065ca5c8049
Wakman Safi <s_safiw21@stud.hwr-berlin.de>
2024-02-05 19:51:10 +0100

    add user model

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from bcrypt import Bcrypt
+
 
 app = Flask(__name__)
+Bcrypt(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'secret'  # adjust this later
 
 db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(820), nullable=False)
 
 
 @app.route('/')
